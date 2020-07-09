79210cb5f07e95c973511d0bcdad448576061c2d
Fabio Espinosa <fa.espinosa10@uniandes.edu.co>
2020-07-09 18:31:59 +0200

    Add OMS, GUI, RR run links

--- config.js
+++ config.js
@@ -3,8 +3,8 @@ const config = {
   Twiki: [
     {
       id: 'CMS_Twiki',
-      links: [{ link: 'https://twiki.cern.ch/twiki/bin/viewauth/CMS/DQM' }]
-    }
+      links: [{ link: 'https://twiki.cern.ch/twiki/bin/viewauth/CMS/DQM' }],
+    },
   ],
   Run_Registry: [
     {
@@ -12,65 +12,81 @@ const config = {
       links: [
         {
           name: 'Prod Online',
-          link: 'https://cmsrunregistry.web.cern.ch/online',
-          run_link: run_number => `/runs/all?run_number=${run_number}`
+          link: 'https://cmsrunregistry.web.cern.ch/online/global',
+          run_link: (run_number) =>
+            `?obf%5Brules%5D%5B0%5D%5Bfield%5D=run_number&obf%5Brules%5D%5B0%5D%5Bvalue%5D=${run_number}&obf%5Brules%5D%5B0%5D%5Boperator%5D=%3D&obf%5Bcombinator%5D=and&obf%5Bnot%5D=false&otf%5Brules%5D%5B0%5D%5Bfield%5D=run_number&otf%5Brules%5D%5B0%5D%5Bvalue%5D=${run_number}&otf%5Brules%5D%5B0%5D%5Boperator%5D=%3D&otf%5Bcombinator%5D=and&otf%5Bnot%5D=false`,
         },
         {
           name: 'Prod Offline',
-          link: 'https://cmsrunregistry.web.cern.ch/offline',
-          run_link: run_number => `/workspaces/global?run_number=${run_number}`
+          link: 'https://cmsrunregistry.web.cern.ch/offline/datasets/global',
+          run_link: (run_number) =>
+            `?oftf%5Brules%5D%5B0%5D%5Bfield%5D=run_number&oftf%5Brules%5D%5B0%5D%5Bvalue%5D=${run_number}&oftf%5Brules%5D%5B0%5D%5Boperator%5D=%3D&oftf%5Bcombinator%5D=and&oftf%5Bnot%5D=false&ofbf%5Brules%5D%5B0%5D%5Bfield%5D=run_number&ofbf%5Brules%5D%5B0%5D%5Bvalue%5D=${run_number}&ofbf%5Brules%5D%5B0%5D%5Boperator%5D=%3D&ofbf%5Bcombinator%5D=and&ofbf%5Bnot%5D=false`,
         },
         {
           name: 'Staging',
-          link: 'https://dev-cmsrunregistry.web.cern.ch'
-        }
-      ]
+          link: 'https://dev-cmsrunregistry.web.cern.ch',
+        },
+      ],
     },
     {
       id: 'Old_Run_Registry',
       links: [
         {
           name: 'Online',
-          link: 'https://cmsrunregistry.cern.ch/runregistry/index.jsf'
+          link: 'https://cmsrunregistry.cern.ch/runregistry/index.jsf',
         },
         {
           name: 'Offline',
-          link: 'https://cmsrunregistry.cern.ch/runregistry_offline/'
-        }
-      ]
-    }
+          link: 'https://cmsrunregistry.cern.ch/runregistry_offline',
+        },
+      ],
+    },
   ],
   DQM_GUI: [
     {
       id: 'NEW_DQM_GUI_(beta)',
       links: [
-        { name: 'Offline', link: 'https://dqm-gui.web.cern.ch'}
-      ]
+        {
+          name: 'Offline',
+          link: 'https://dqm-gui.web.cern.ch',
+          run_link: (run_number) =>
+            `?search_run_number=${run_number}&search_dataset_name=`,
+        },
+      ],
     },
     {
       id: 'DQM_GUI',
       links: [
         { name: 'Online', link: 'https://cmsweb.cern.ch/dqm/online' },
-        { name: 'offline', link: 'https://cmsweb.cern.ch/dqm/offline/' }
-      ]
-    }
+        {
+          name: 'offline_gui',
+          link: 'https://cmsweb.cern.ch/dqm/offline',
+          run_link: (run_number) => `/start?runnr=${run_number};`,
+        },
+      ],
+    },
   ],
   Historic_DQM: [
     {
       id: 'Historic_DQM',
-      links: [{ link: 'https://cms-hdqm.web.cern.ch/' }
-      ]
-    }
+      links: [
+        {
+          name: 'hdqm',
+          link: 'https://cms-hdqm.web.cern.ch',
+          run_link: (run_number) => `?filter=list&filterValue=${run_number}`,
+        },
+      ],
+    },
   ],
   Meetings: [
     {
       id: 'DQM_Meetings',
-      links: [{ link: 'https://indico.cern.ch/category/3904/' }]
+      links: [{ link: 'https://indico.cern.ch/category/3904/' }],
     },
     {
       id: 'Run_Meetings',
-      links: [{ link: 'https://indico.cern.ch/event/402363' }]
-    }
+      links: [{ link: 'https://indico.cern.ch/event/402363' }],
+    },
   ],
   Run_Coordination: [
     {
@@ -78,10 +94,10 @@ const config = {
       links: [
         {
           link:
-            'http://schwick.web.cern.ch/schwick/rctools/dailyReport/listReports/'
-        }
-      ]
-    }
+            'http://schwick.web.cern.ch/schwick/rctools/dailyReport/listReports/',
+        },
+      ],
+    },
   ],
   WBM: [
     {
@@ -89,23 +105,29 @@ const config = {
       links: [
         {
           name: 'Primary',
-          link: 'https://cmswbm.cern.ch/'
+          link: 'https://cmswbm.cern.ch/',
         },
         {
           name: 'Secondary',
-          link: 'https://cmswbm2.cern.ch'
-        }
-      ]
+          link: 'https://cmswbm2.cern.ch',
+        },
+      ],
     },
     {
       id: 'WBM Offline',
-      links: [{ link: 'https://cmswbmoff.web.cern.ch/cmswbmoff/' }]
-    }
+      links: [{ link: 'https://cmswbmoff.web.cern.ch/cmswbmoff/' }],
+    },
   ],
   OMS: [
     {
       id: 'OMS',
-      links: [{ link: 'https://cmsoms.cern.ch/cms/index/index' }]
-    }
-  ]
+      links: [
+        {
+          name: 'OMS',
+          link: 'https://cmsoms.cern.ch/cms/index/index',
+          run_link: (run_number) => `?cms_run=${run_number}`,
+        },
+      ],
+    },
+  ],
 };
