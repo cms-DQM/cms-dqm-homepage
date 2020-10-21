11d9bf917bb8418246caac75d60e91118b8d8c22
Fabio <fa.espinosa10@uniandes.edu.co>
2020-10-21 15:14:01 +0200

    add external links

--- config.js
+++ config.js
@@ -1,132 +1,145 @@
+// There are two items, external (goes on the left, and dqm_links goes on the middle)
 // Every property is a service category with an array of services/links.
 const config = {
-  Twiki: [
-    {
-      id: 'CMS_Twiki',
-      links: [{ link: 'https://twiki.cern.ch/twiki/bin/viewauth/CMS/DQM' }],
-    },
-  ],
-  Run_Registry: [
-    {
-      id: 'New_Run_Registry',
-      links: [
-        {
-          name: 'Prod Online',
-          link: 'https://cmsrunregistry.web.cern.ch/online/global',
-          run_link: (run_number) =>
-            `?obf%5Brules%5D%5B0%5D%5Bfield%5D=run_number&obf%5Brules%5D%5B0%5D%5Bvalue%5D=${run_number}&obf%5Brules%5D%5B0%5D%5Boperator%5D=%3D&obf%5Bcombinator%5D=and&obf%5Bnot%5D=false&otf%5Brules%5D%5B0%5D%5Bfield%5D=run_number&otf%5Brules%5D%5B0%5D%5Bvalue%5D=${run_number}&otf%5Brules%5D%5B0%5D%5Boperator%5D=%3D&otf%5Bcombinator%5D=and&otf%5Bnot%5D=false`,
-        },
-        {
-          name: 'Prod Offline',
-          link: 'https://cmsrunregistry.web.cern.ch/offline/datasets/global',
-          run_link: (run_number) =>
-            `?oftf%5Brules%5D%5B0%5D%5Bfield%5D=run_number&oftf%5Brules%5D%5B0%5D%5Bvalue%5D=${run_number}&oftf%5Brules%5D%5B0%5D%5Boperator%5D=%3D&oftf%5Bcombinator%5D=and&oftf%5Bnot%5D=false&ofbf%5Brules%5D%5B0%5D%5Bfield%5D=run_number&ofbf%5Brules%5D%5B0%5D%5Bvalue%5D=${run_number}&ofbf%5Brules%5D%5B0%5D%5Boperator%5D=%3D&ofbf%5Bcombinator%5D=and&ofbf%5Bnot%5D=false`,
-        },
-        {
-          name: 'Staging',
-          link: 'https://dev-cmsrunregistry.web.cern.ch',
-        },
-      ],
-    },
-    {
-      id: 'Old_Run_Registry',
-      links: [
-        {
-          name: 'Online',
-          link: 'https://cmsrunregistry.cern.ch/runregistry/index.jsf',
-        },
-        {
-          name: 'Offline',
-          link: 'https://cmsrunregistry.cern.ch/runregistry_offline',
-        },
-      ],
-    },
-  ],
-  DQM_GUI: [
-    {
-      id: 'NEW_DQM_GUI_(beta)_ONLY_OFFLINE',
-      links: [
-        {
-          name: 'Offline',
-          link: 'https://dqm-gui.web.cern.ch',
-          run_link: (run_number) =>
-            `?search_run_number=${run_number}&search_dataset_name=`,
-        },
-      ],
-    },
-    {
-      id: 'DQM_GUI',
-      links: [
-        { name: 'Online', link: 'https://cmsweb.cern.ch/dqm/online', run_link: (run_number) => `/start?runnr=${run_number};dataset=/Global/Online/ALL;sampletype=online_data;workspace=Everything` },
-        {
-          name: 'offline_gui',
-          link: 'https://cmsweb.cern.ch/dqm/offline',
-        },
-      ],
-    },
-  ],
-  Historic_DQM: [
-    {
-      id: 'Historic_DQM',
-      links: [
-        {
-          name: 'hdqm',
-          link: 'https://cms-hdqm.web.cern.ch',
-          run_link: (run_number) => `?filter=list&filterValue=${run_number}`,
-        },
-      ],
-    },
-  ],
-  Meetings: [
-    {
-      id: 'DQM_Meetings',
-      links: [{ link: 'https://indico.cern.ch/category/3904/' }],
-    },
-    {
-      id: 'Daily_Run_Meetings',
-      links: [{ link: 'https://indico.cern.ch/event/402363' }],
-    },
-  ],
-  Run_Coordination: [
-    {
-      id: 'Run_Coordination',
-      links: [
-        {
-          link:
-            'http://schwick.web.cern.ch/schwick/rctools/dailyReport/listReports/',
-        },
-      ],
-    },
-  ],
-  WBM: [
-    {
-      id: 'WBM',
-      links: [
-        {
-          name: 'Primary',
-          link: 'https://cmswbm.cern.ch/',
-        },
-        {
-          name: 'Secondary',
-          link: 'https://cmswbm2.cern.ch',
-        },
-      ],
-    },
-    {
-      id: 'WBM Offline',
-      links: [{ link: 'https://cmswbmoff.web.cern.ch/cmswbmoff/' }],
-    },
-  ],
-  OMS: [
-    {
-      id: 'OMS',
-      links: [
-        {
-          name: 'OMS',
-          link: 'https://cmsoms.cern.ch',
-          run_link: (run_number) => `/cms/runs/report?cms_run=${run_number}`,
-        },
-      ],
-    },
-  ],
+  external: {
+    Meetings: [
+      {
+        id: 'DQM_Meetings',
+        links: [{ link: 'https://indico.cern.ch/category/3904/' }],
+      },
+      {
+        id: 'Daily_Run_Meetings',
+        links: [{ link: 'https://indico.cern.ch/event/402363' }],
+      },
+    ],
+    Run_Coordination: [
+      {
+        id: 'Run_Coordination',
+        links: [
+          {
+            link:
+              'http://schwick.web.cern.ch/schwick/rctools/dailyReport/listReports/',
+          },
+        ],
+      },
+    ],
+    OMS: [
+      {
+        id: 'OMS',
+        links: [
+          {
+            name: 'OMS',
+            link: 'https://cmsoms.cern.ch',
+            run_link: (run_number) => `/cms/runs/report?cms_run=${run_number}`,
+          },
+        ],
+      },
+    ],
+    Elog: [
+      {
+        id: 'Elog',
+        links: [
+          {
+            link:
+              'https://cmsonline.cern.ch/webcenter/portal/cmsonline/pages_common/elog?__adfpwp_action_portlet=683379043&__adfpwp_backurl=https%3A%2F%2Fcmsonline.cern.ch%3A443%2Fwebcenter%2Fportal%2Fcmsonline%2Fpages_common%2Felog%3F_afrRedirect%3D9715040540107362%26__adfpwp_mode.623564097%3D1&_piref683379043.strutsAction=%2FviewSubcatMessages.do%3FcatId%3D493%26subId%3D20%26page%3D1',
+          },
+        ],
+      },
+    ],
+    E_groups: [
+      {
+        id: 'E-groups',
+        links: [
+          {
+            link: 'https://e-groups.cern.ch/e-groups/EgroupsSearchForm.do',
+          },
+        ],
+      },
+    ],
+    Web_Mail: [{ id: 'Web_Mail', links: [{ link: 'https://mmm.cern.ch/' }] }],
+  },
+  dqm_links: {
+    Twiki: [
+      {
+        id: 'CMS_DQM_Main_Twiki',
+        links: [{ link: 'https://twiki.cern.ch/twiki/bin/viewauth/CMS/DQM' }],
+      },
+    ],
+    Run_Registry: [
+      {
+        id: 'New_Run_Registry',
+        links: [
+          {
+            name: 'Prod Online',
+            link: 'https://cmsrunregistry.web.cern.ch/online/global',
+            run_link: (run_number) =>
+              `?obf%5Brules%5D%5B0%5D%5Bfield%5D=run_number&obf%5Brules%5D%5B0%5D%5Bvalue%5D=${run_number}&obf%5Brules%5D%5B0%5D%5Boperator%5D=%3D&obf%5Bcombinator%5D=and&obf%5Bnot%5D=false&otf%5Brules%5D%5B0%5D%5Bfield%5D=run_number&otf%5Brules%5D%5B0%5D%5Bvalue%5D=${run_number}&otf%5Brules%5D%5B0%5D%5Boperator%5D=%3D&otf%5Bcombinator%5D=and&otf%5Bnot%5D=false`,
+          },
+          {
+            name: 'Prod Offline',
+            link: 'https://cmsrunregistry.web.cern.ch/offline/datasets/global',
+            run_link: (run_number) =>
+              `?oftf%5Brules%5D%5B0%5D%5Bfield%5D=run_number&oftf%5Brules%5D%5B0%5D%5Bvalue%5D=${run_number}&oftf%5Brules%5D%5B0%5D%5Boperator%5D=%3D&oftf%5Bcombinator%5D=and&oftf%5Bnot%5D=false&ofbf%5Brules%5D%5B0%5D%5Bfield%5D=run_number&ofbf%5Brules%5D%5B0%5D%5Bvalue%5D=${run_number}&ofbf%5Brules%5D%5B0%5D%5Boperator%5D=%3D&ofbf%5Bcombinator%5D=and&ofbf%5Bnot%5D=false`,
+          },
+          {
+            name: 'Staging',
+            link: 'https://dev-cmsrunregistry.web.cern.ch',
+          },
+        ],
+      },
+      {
+        id: 'Old_Run_Registry',
+        links: [
+          {
+            name: 'Online',
+            link: 'https://cmsrunregistry.cern.ch/runregistry/index.jsf',
+          },
+          {
+            name: 'Offline',
+            link: 'https://cmsrunregistry.cern.ch/runregistry_offline',
+          },
+        ],
+      },
+    ],
+    DQM_GUI: [
+      {
+        id: 'NEW_DQM_GUI_(beta)_ONLY_OFFLINE',
+        links: [
+          {
+            name: 'Offline',
+            link: 'https://dqm-gui.web.cern.ch',
+            run_link: (run_number) =>
+              `?search_run_number=${run_number}&search_dataset_name=`,
+          },
+        ],
+      },
+      {
+        id: 'DQM_GUI',
+        links: [
+          {
+            name: 'Online',
+            link: 'https://cmsweb.cern.ch/dqm/online',
+            run_link: (run_number) =>
+              `/start?runnr=${run_number};dataset=/Global/Online/ALL;sampletype=online_data;workspace=Everything`,
+          },
+          {
+            name: 'offline_gui',
+            link: 'https://cmsweb.cern.ch/dqm/offline',
+          },
+        ],
+      },
+    ],
+    Historic_DQM: [
+      {
+        id: 'Historic_DQM',
+        links: [
+          {
+            name: 'hdqm',
+            link: 'https://cms-hdqm.web.cern.ch',
+            run_link: (run_number) => `?filter=list&filterValue=${run_number}`,
+          },
+        ],
+      },
+    ],
+  },
 };
--- main.js
+++ main.js
@@ -1,6 +1,13 @@
 // Go around config file and fill Links:
 const link_list = document.querySelector('#link_list');
-for (let [key, val] of Object.entries(config)) {
+const external_list = document.querySelector('#external_links');
+
+addLinks(config.external, 'external_links');
+addLinks(config.dqm_links, 'link_list');
+
+function addLinks(link_object, ul_id) {
+  const link_list = document.querySelector(`#${ul_id}`);
+  for (let [key, val] of Object.entries(link_object)) {
     key = key.split('_').join(' ');
     const service = document.createElement('li');
     const service_title = document.createElement('h6');
@@ -12,71 +19,69 @@ for (let [key, val] of Object.entries(config)) {
 
     const inner_services = fill_services(val);
     service.appendChild(inner_services);
+  }
 }
 
 // Returns UL with service list
 function fill_services(array) {
-    const service_list = document.createElement('ul');
-    array.forEach(({ id, links }) => {
-        const inner_service = document.createElement('li');
-        service_list.appendChild(inner_service);
+  const service_list = document.createElement('ul');
+  array.forEach(({ id, links }) => {
+    const inner_service = document.createElement('li');
+    service_list.appendChild(inner_service);
 
-        if (links.length > 1) {
-            const inner_service_title = document.createTextNode(
-                id.split('_').join(' ')
-            );
-            inner_service.appendChild(inner_service_title);
-            links.forEach(({ name, link }) => {
-                const new_link = document.createElement('a');
-                const new_link_name = document.createTextNode(`[${name}]`);
-                const space = document.createTextNode(' ');
+    if (links.length > 1) {
+      const inner_service_title = document.createTextNode(
+        id.split('_').join(' ')
+      );
+      inner_service.appendChild(inner_service_title);
+      links.forEach(({ name, link }) => {
+        const new_link = document.createElement('a');
+        const new_link_name = document.createTextNode(`[${name}]`);
+        const space = document.createTextNode(' ');
 
-                inner_service.appendChild(space);
-                new_link.href = link;
-                new_link.appendChild(new_link_name);
-                inner_service.appendChild(new_link);
-            });
-        } else {
-            const new_link = document.createElement('a');
-            const new_link_name = document.createTextNode(
-                id.split('_').join(' ')
-            );
-            new_link.href = links[0].link;
-            new_link.appendChild(new_link_name);
-            inner_service.appendChild(new_link);
-        }
-    });
-    return service_list;
+        inner_service.appendChild(space);
+        new_link.href = link;
+        new_link.appendChild(new_link_name);
+        inner_service.appendChild(new_link);
+      });
+    } else {
+      const new_link = document.createElement('a');
+      const new_link_name = document.createTextNode(id.split('_').join(' '));
+      new_link.href = links[0].link;
+      new_link.appendChild(new_link_name);
+      inner_service.appendChild(new_link);
+    }
+  });
+  return service_list;
 }
 
 const run_number_input = document.querySelector('#run_number');
-run_number_input.addEventListener('keyup', event => {
-    // Remove previous elements
-    document.querySelector('#run_number_dependent_services').innerHTML = '';
+run_number_input.addEventListener('keyup', (event) => {
+  // Remove previous elements
+  document.querySelector('#run_number_dependent_services').innerHTML = '';
 
-    const run_number = event.target.value;
-    if (run_number.length > 0) {
-        const list = document.querySelector('#run_number_dependent_services');
-        for (const [key, category] of Object.entries(config)) {
-            category.forEach(({ id, links }) => {
-                id = id.split('_').join(' ');
-                links.forEach(({ name, link, run_link }) => {
-                    if (run_link) {
-                        const run_dependant_service = document.createElement(
-                            'li'
-                        );
-                        const new_link = document.createElement('a');
-                        const new_link_name = document.createTextNode(
-                            `${id} - ${name} - Run ${run_number}`
-                        );
-                        new_link.target = '__blank';
-                        new_link.href = `${link}${run_link(run_number)}`;
-                        new_link.appendChild(new_link_name);
-                        run_dependant_service.appendChild(new_link);
-                        list.appendChild(run_dependant_service);
-                    }
-                });
-            });
-        }
+  const run_number = event.target.value;
+  if (run_number.length > 0) {
+    const list = document.querySelector('#run_number_dependent_services');
+    const all_services = { ...config.external, ...config.dqm_links };
+    for (const [key, category] of Object.entries(all_services)) {
+      category.forEach(({ id, links }) => {
+        id = id.split('_').join(' ');
+        links.forEach(({ name, link, run_link }) => {
+          if (run_link) {
+            const run_dependant_service = document.createElement('li');
+            const new_link = document.createElement('a');
+            const new_link_name = document.createTextNode(
+              `${id} - ${name} - Run ${run_number}`
+            );
+            new_link.target = '__blank';
+            new_link.href = `${link}${run_link(run_number)}`;
+            new_link.appendChild(new_link_name);
+            run_dependant_service.appendChild(new_link);
+            list.appendChild(run_dependant_service);
+          }
+        });
+      });
     }
+  }
 });
