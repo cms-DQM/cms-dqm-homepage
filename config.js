02dc4788cd24780497b5b6b25a38406a79fece33
Dimitris Papagiannis <d.papag.a@gmail.com>
2023-09-14 10:47:14 +0200

    Update config.js

--- config.js
+++ config.js
@@ -161,6 +161,23 @@ const config = {
         id: 'DQM_Squared_Mirror',
         links: [
           {
+            name: 'DQM^2 Mirror@P5',
+            link: 'https://cmsweb.cern.ch/dqm/dqm-square/',
+            run_link: (run_number) => `tmp/content_parser_productionPARSER_run${run_number}`, 
+          },
+          {
+            name: 'DQM^2 Mirror@CMSWEB k8s,
+            link: 'https://cmsweb.cern.ch/dqm/dqm-square-k8/',
+            run_link: (run_number) => `?run=${run_number}&db=production`, 
+          },
+        ],
+      },
+    ],
+    PR_Inspector: [
+      {
+        id: 'PR_Inspector',
+        links: [
+          {
             name: 'mDQM^2',
             link: 'https://cmsweb.cern.ch/dqm/dqm-square/',
             run_link: (run_number) => `tmp/content_parser_productionPARSER_run${run_number}`, 
